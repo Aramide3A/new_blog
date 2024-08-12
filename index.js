4a03ab35215e7ae4ffebe6419273d45bf58c7b3b
Al-Ameen Ameen <alameenameen3101@gmail.com>
2024-08-12 08:31:30 +0100

    update cors

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const swaggerUI = require('swagger-ui-express')
 const specs = require('./swagger')
 const cors = require('cors')
 
+app.use(cors({ origin: ['http://localhost:3000','https://blog-mu-inky-78.vercel.app/']}));
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:3000', 'https://blog-mu-inky-78.vercel.app/']}));
 app.use('/public', express.static('public'));
 
 // API Documentation
